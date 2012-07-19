efc6566dbab0e1005d5ece8178e475993995355a
Thomas Bancel <bancel.thomas@gmail.com>
2012-07-19 13:49:31 +0200

    Finished exercices chap5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,28 +3,52 @@ require 'spec_helper'
 describe "Static pages" do
   subject{page}
   
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    #page.should
+    click_link "Contact"
+    #page.should
+    click_link "Home"
+    click_link "Sign up now!"
+    #page.should
+    click_link "Surfing app"
+    #page.should  
+  end
+  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', :text=>heading)}
+    it { should have_selector('title', :text=>full_title(page_title))}
+  end
+  
   describe "Home Page" do
     before{visit root_path}    
-    it { should have_selector('h1', :text=>"Sample App")}
-    it { should have_selector('title', :text=>full_title(''))}
+    let(:heading) {'Sample App'}
+    let(:page_title) {''}
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', :text=>"| Home")}
   end
   
   describe "Help Page" do
     before{visit help_path}
-    it { should have_selector('h1', :text=>"Help")}
-    it { should have_selector('title', :text=>full_title('Help'))}
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About" do
     before{visit about_path}
-    it { should have_selector('h1', :text=>"About Us")}
-    it { should have_selector('title', :text=>full_title('About'))}
+    let(:heading) {'About'}
+    let(:page_title) {'About'}
+    it_should_behave_like "all static pages"
   end
  
   describe "Contact" do
     before{visit contact_path}
-    it { should have_selector('h1', :text=>"Contact")}
-    it { should have_selector('title', :text=>full_title('Contact'))}
+    let(:heading) {'Contact us'}
+    let(:page_title) {'Contact'}
+    it_should_behave_like "all static pages"
   end 
 end
