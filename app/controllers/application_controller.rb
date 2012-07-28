616b946c91a82bfe51c3fb6215a144bd9f78b8a1
Thomas Bancel <bancel.thomas@gmail.com>
2012-07-28 23:00:08 +0200

    Finish password website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  
+  http_basic_authenticate_with :name => "thomasbancel", :password => "moneymoney"
 end
