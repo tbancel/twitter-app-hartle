1a935a3d19ed0b5785f560447276076c73aa8caf
Thomas Bancel <bancel.thomas@gmail.com>
2013-01-21 22:30:18 +0100

    Corrected errors in exception_message.

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,11 +1,11 @@
 class ErrorMailer < ActionMailer::Base
   default :from => "support@fidbacks.com"
   
-  def exception_message(exception, request.env)
-    @exception=exception
-    @environment=environment
-    @controller=
-    @backtrace=exception.backtrace
-    mail(:to => "thomas.bancel@fidbacks.com", :subject => "Error Raised")
-  end
+  # def exception_message(exception, request)
+  #   @exception=exception
+  #   @environment=environment
+  #   @controller=
+  #   @backtrace=exception.backtrace
+  #   mail(:to => "thomas.bancel@fidbacks.com", :subject => "Error Raised")
+  # end
 end
